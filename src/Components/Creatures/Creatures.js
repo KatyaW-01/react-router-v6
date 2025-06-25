cd98d074131ba35d8b75d2d9616c8a2da29cb4a7
Katya Weicht <katya.weicht@gmail.com>
2025-06-24 17:27:36 -0700

    Render the animal photos on the webpage

--- src/Components/Creatures/Creatures.js
+++ src/Components/Creatures/Creatures.js
@@ -1,15 +1,22 @@
 import './Creatures.css';
 import {useParams} from "react-router-dom"
+import { getCreaturesData } from "../../data/animalData"
 
 const Creatures = ({  }) => {
   const creatureType = useParams().creatureType
   console.log("creature type:",creatureType)
   console.log("use Params:",useParams())
 
+  const creatureImages = getCreaturesData(creatureType).map(creature => {
+    const { id, image } = creature;
+    return <img src={image} key={id} id={id} className="app-img"/>
+  });
+
   return (
-    <div>
-      <h1>{`${creatureType.toUpperCase()}!`}</h1>
-    </div>
+    <>
+      <h1>{creatureType.toUpperCase()}!</h1>
+      {creatureImages}
+    </>
   )
 }
 
